d02f7521109b3193c459fb77b67acedf2eefaae4
Santiago Lopez <slopez@fi.uba.ar>
2024-05-16 23:57:32 -0300

    Close all open connections on shutdown.

--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,7 @@
 import concurrent.futures
 import signal
 import socket
+import request_parser
 
 class Server:
     _BUFSIZE = 10000
@@ -21,12 +22,17 @@ class Server:
             while True:
                 # Receive incoming messages and submit them to the thread pool
                 (client_skt, addr) = self._skt.accept()
+                self._active_clients.append(client_skt)
                 data = self._skt.recv(self._BUFSIZE)
                 future = executor.submit(self.handle_request, data, client_skt)
                 future.add_done_callback(lambda f: f.result())
 
     def handle_request(self, data, client_skt):
+        # TODO: handle request.
+
+        # Close client connection after handling the request.
         client_skt.close()
+        _active_clients.remove(client_skt)
 
     def _activate_sigint_handler(self):
         signal.signal(signal.SIGINT, self._signal_handler)
@@ -34,6 +40,8 @@ class Server:
     def _signal_handler(self):
         res = input("Do you really want to exit? y/n ")
         if res == "y":
+            for client in self._active_clients:
+                client.close()
             self._skt.close()
             exit(1)
 
