b3cb973c4fc6a160be0b691ff0dfde55cb6f734a
noahmasri <mmasri@fi.uba.ar>
2024-05-27 10:58:28 -0300

    delete prints

--- src/mainServer.py
+++ src/mainServer.py
@@ -4,9 +4,11 @@ import requests
 from urllib.parse import urlparse
 
 class Handler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        parsed_path = urlparse(self.path)
 
+    # change back to post, but set get so as to be able to access via internet
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        print(parsed_path)
         # could be more object oriented but not feeling like it,
         # wont add any more endpoints ever
         if parsed_path.path != "/secret" and parsed_path.path != "/image":
@@ -20,26 +22,30 @@ class Handler(BaseHTTPRequestHandler):
 
     def handle_image_req(self):
         # read the body of the request
+        if 'Content-Length' not in self.headers:
+            self.send_user_error(b'Incomplete request: something in request body is missing')
+            return 
+        
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode('utf-8')
 
         print(post_data)
         
-        # only allows your request if you have "urlToRedirect=url" as he does not handle images 
+        # only allows your request if you have "urlToRedirect=url" as 
+        # this server does not handle images 
         prefix = "urlToRedirect="
         if post_data.startswith(prefix):
             # extracts value of urlToRedirect field (awfully but works)
-            url_to_redirect = post_data.split('\n', 1)[0].replace(prefix, "").strip()
+            url_to_redirect = post_data.split('\n', 1)[0].replace(prefix, "").replace('"', "").strip()
             print(url_to_redirect)
             try:
                 response = requests.get(url_to_redirect)
-                
-                # resend to 
+                # resend 
                 self.send_response(response.status_code)
-                self.send_header('Content-type', response.headers['Content-Type'])
+                if 'Content-type' in  response.headers:
+                    self.send_header('Content-type', response.headers['Content-Type'])
                 self.end_headers()
                 self.wfile.write(response.content)
-                print(response.content)
 
             except requests.RequestException as e:
                 # could not do get request properly
@@ -48,17 +54,28 @@ class Handler(BaseHTTPRequestHandler):
                 self.wfile.write(f'Error fetching URL: {e}'.encode('utf-8'))
         
         else:
-            # if there is no url to redirect, send user error
-            self.send_response(400)
-            self.end_headers()
-            self.wfile.write(b'Missing a url to request images')
+            self.send_user_error(b'Missing a url to request images')
+            
+    def send_user_error(self, msg):
+        self.send_response(400)
+        self.end_headers()
+        self.wfile.write(msg)
 
     def handle_secret(self):
-        # should not be accesible if not in LAN
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Dang it, you hacked me!!')
+        # endpoint should not be accesible if not in LAN
 
+        # if has a forwarded host, then comes from ngrok tunel
+        if 'X-Forwarded-Host' not in self.headers:
+            print("hacked")
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'Dang it, you hacked me!!')
+        else:
+            print("firewalled")
+            self.send_response(403)
+            self.end_headers()
+            self.wfile.write(b'Forbidden')
+    
 class ThreadedHTTPServer(ThreadingMixIn,HTTPServer):
     """ handles requests in separate threads """
     # ver como lograr que con CTRL + C no se rompa todo sino que termine bien
